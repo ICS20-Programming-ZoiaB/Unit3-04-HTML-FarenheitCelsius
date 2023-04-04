aeecc94c9b26ef7223a0aa4bee2a90083b0b3526
ZoiaBaran <124380748+ZoiaBaran@users.noreply.github.com>
2023-04-04 14:37:04 +0000

    Added code

---
+++ js/script.js
@@ -0,0 +1,21 @@
+// Copyright (c) 2023 ZoiaB All rights reserved
+//
+// Created by: ZoiaB
+// Created on: Apr 2023
+// This file contains the JS functions for index.html
+
+"use strict"
+
+// Function
+function temperatureEntered() {
+
+// Get user input
+  let fahrenheit = parseFloat(document.getElementById("temp").value);
+
+// Calculation
+  let celsius = 5 / 9 * (fahrenheit - 32);
+  let celsiusRounded = celsius.toFixed(1)
+
+// Display results
+  document.getElementById("results").innerHTML = "The temperature in Celsius is " + celsiusRounded + " degrees Celsius."
+}
